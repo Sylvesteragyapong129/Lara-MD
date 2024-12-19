ddb411dde662d90dd759ec1b2885bdfe4ad6d245
Sylvesteragyapong129 <sylvesteragyapong129@gmail.com>
2024-12-19 07:09:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "X6oGmLyJ#SDfUlJnquIuilBSZUvFeV0dCdezeweJCU6icFtBuHn0", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "private",
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
